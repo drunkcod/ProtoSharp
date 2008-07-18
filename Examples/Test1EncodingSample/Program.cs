50184cce8a2247f47cbdcc3749b4ec61f08691c4
Torbjorn Gyllebring <i.am@drunkencoder.com>
2008-07-18 08:08:52 +0000

    Test1EncodingSample added.

---
+++ Examples/Test1EncodingSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using ProtoSharp.Core;
+
+//Based on http://code.google.com/apis/protocolbuffers/docs/encoding.html "A Simple Message"
+namespace Test1EncodingSample
+{
+    class Test1
+    {
+        [Tag(1)]
+        public int A { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var encoded = new MemoryStream();
+            new MessageWriter(encoded).WriteMessage(new Test1() { A = 150 });
+            Console.Write("Encoded = {");//Expected 08 96 01
+            var sep = string.Empty;
+            Array.ForEach(encoded.ToArray(), b =>
+            {
+                Console.Write("{0}{1:x}", sep, b);
+                sep = ", ";
+            });
+            Console.WriteLine("}");
+
+            var decoded = MessageReader.Read<Test1>(encoded.ToArray());
+            Console.WriteLine("Decoded = {{A: {0}}}", decoded.A);
+        }
+    }
+}
