481d09649784288701a0b3cb638e8ca73bf3e9c1
Torbjorn Gyllebring <i.am@drunkencoder.com>
2008-07-22 22:41:02 +0000

    UInt32 + SInt32 performance tweaks.

--- Source/Core/MessageFieldSInt32.cs
+++ Source/Core/MessageFieldSInt32.cs
@@ -8,10 +8,16 @@ namespace ProtoSharp.Core
         public MessageFieldSInt32(int tag, IFieldIO fieldIO): base(tag, fieldIO, WireType.Varint){}
 
         protected override bool CanAppendWriteCore { get { return true; } }
+        protected override bool CanAppendReadCore { get { return true; } }
 
         public override void AppendWriteField(ILGenerator il)
         {
             il.Emit(OpCodes.Call, typeof(MessageWriter).GetMethod("WriteZigZag", new Type[] { typeof(int) }));
+        }
+
+        public override void AppendReadField(ILGenerator il)
+        {
+            il.Emit(OpCodes.Call, typeof(MessageReader).GetMethod("ReadZigZag32", Type.EmptyTypes));
         }
 
         protected override object DoRead(MessageReader reader)
--- Source/Core/MessageFieldUInt32.cs
+++ Source/Core/MessageFieldUInt32.cs
@@ -7,6 +7,13 @@ namespace ProtoSharp.Core
     {
         public MessageFieldUInt32(int tag, IFieldIO fieldIO) : base(tag, fieldIO) { }
 
+        protected override bool CanAppendReadCore { get { return true; } }
+
+        public override void AppendReadField(ILGenerator il)
+        {
+            il.Emit(OpCodes.Call, typeof(MessageReader).GetMethod("ReadVarint32", Type.EmptyTypes));
+        }
+
         protected override object DoRead(MessageReader reader)
         {
             return (uint)reader.ReadVarint32();
